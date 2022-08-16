306e1f6d39ff740edfa20d335b3d88c780de5f26
JBritton2343 <jbritton854@gmail.com>
2022-08-16 18:32:11 -0500

    added new function to app.js

--- app.js
+++ app.js
@@ -1,5 +1,42 @@
-const sum = (a,b)=>{
-    return a + b
+const fromEuroToDollar = function(valueInEuro){
+    // convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.2;
+    //return the dollar value
+    return valueInDollar;
 }
-console.log(sum(7,3))
-module.exports={sum};
+module.exports = {sum, fromEuroToDollar};
+
+
+//const sum = (a,b)=>{
+    return a + b
+//}
+//console.log(sum(7,3))
+//module.exports={sum};//
+
+let oneEuroIs = {
+    "JPY": 127.9, //Japanese Yen
+    "USD": 1.2, //US Dollar
+    "GBP": 0.8, //British Pound
+}
+const fromDollarToYen = (a)=>{
+    let a = amountDollars;
+    let b = 1.2;
+    let c = 127.9;
+    return a * b / c;
+};
+console.log(fromDollarToYen(9));
+
+const fromEuroToDollar = (a)=>{
+    let a = amountEuros;
+    let b = 1.2;
+    return a / b;
+};
+console.log(fromEuroToDollar(12));
+
+const fromYenToPound = (a)=>{
+    let a = amountYen;
+    let b = 127.9;
+    let c = .8;
+    return a * b / c
+};
+console.log(fromYenToPound(120000));
