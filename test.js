48fe2798386715a37a6b3a27f30b6965b5ab4cd7
JBritton2343 <jbritton854@gmail.com>
2022-08-19 19:12:22 -0500

    updated tests

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const { sum } = require('./app.js');
+const { sum, fromEuroToDollar } = require('./app.js');
 //first test
 test('adds 14 + 9 to equal 23', ()=>{
 let total=sum(14, 9);
@@ -16,19 +16,22 @@ test("One euro should be 1.206 dollars", function(){
     
     // this is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
-})
+});
 test("One Euro should be 1.2 dollars", function(){
     // importing function from js file
-    const { fromDollarsToYen } = require('./app.js') 
+    const { fromDollarToYen } = require('./app.js') 
 
     // if one Euro is 1.2 dollars, then 35 euros shouold be (35 * 1.2)
-    let valInDollars = 35 * 1.2;
+    const dollars = fromEuroToDollar(35)
+    
+    expect(fromEuroToDollar(35)).toBe(42);
 
     // with that value set we can now test the coversion to yen
-    valInYen = valInDollars * 127.9;
+    const yen = fromDollarsToYen(42)
+    const expected = 42 *  127.9;
 
     // now we return the value in Yen
-    return valInYen;
+    expect(fromDollarToYen(42)).toBe(5371.8);
 
 
 })
@@ -39,12 +42,14 @@ test("One Euro is should be .8 pounds", function(){
     const{fromYenToPound}=require('./app.js')
 
     //if one Euro is 127.9 Yen, then 10000000 yen should be (100000000 / 127.9)
-    let valueInEuro = 10000000 / 127.9;
+    const yen = fromYenToEuro(10000000)
+    const yenConverted = 10000000 / 127.9 
 
     // with the euros determined we can now convert that to Pounds
-    let valinPounds = valueInEuro / .8;
+    const pounds = fromEuroToPounds(78186.08)
+    const expected = 78186.08 / .8
 
     // now we return the value in Pounds
-    return valinPounds;
+    expect(fromYenToPound(78186.08)).toBe(97732.60)
 
 })
